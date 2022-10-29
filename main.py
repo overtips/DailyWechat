c804bb37fe19e365fea3279de47269e56245bab9
overtips <114346230+overtips@users.noreply.github.com>
2022-10-30 00:22:33 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def get_random_color():
 
 
 def get_weather(city):
-    url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&city=" + '牡丹区'
+    url = "https://www.mxnzp.com/api/weather/current/{牡丹区}" 
     res = requests.get(url).json()
     weather = res['data']['list'][0]
     return weather['weather'], math.floor(weather['high']), math.floor(weather['low']), weather['city']
