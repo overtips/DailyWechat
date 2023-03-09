be576ec6c19d1fdc196a05770e5119e4b8c2b99a
overtips <114346230+overtips@users.noreply.github.com>
2023-03-09 09:11:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_time():
 def get_onetalk():
     url='https://saying.api.azwcl.com/saying/get'
     res = requests.get(url).json()
-    onetalk=res['data']['0']
+    onetalk=res['data'][0]['content']
     return onetalk
 
 def get_random_color():
@@ -68,8 +68,8 @@ for user_info in data:
         'color':'#470024'
         }
     data['onetalk'] = {
-        'value': get_onetalk['content'], 
-        'color': '#92DFB2'
+    'value': get_onetalk()['content'], 
+    'color': '#92DFB2'
         }
     data['weather'] = {
         'value': weather['wea'], 
