3c0436360c7a48f422aff270062051f3ec6ba952
overtips <114346230+overtips@users.noreply.github.com>
2023-03-09 11:03:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_random_color():
 def get_weather(city):
     url = "https://v0.yiketianqi.com/api?unescape=1&version=v63&appid=43656176&appsecret=I42og6Lm&ext=&city=牡丹"
     res = requests.get(url).json()
-    weather = res['data'][0]
+    weather = res['date'][0]
     return weather
 	
 def get_count(born_date):
@@ -54,9 +54,8 @@ for user_info in data:
     city = user_info['city']
     user_id = user_info['user_id']
     name=user_info['user_name'].upper()
-    
-    weather= get_weather(city)
 
+   
     data = dict()
     data['time'] = {
         'value': get_time(), 
